43a398f29f300be9744c6e3008bb2fff85d51ecc
MuKuL-DiXiT <mukuldixit086@gmail.com>
2025-07-02 02:55:08 +0530

    notification

--- frontend/src/components/CommunityPage.jsx
+++ frontend/src/components/CommunityPage.jsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { ImagePlay } from 'lucide-react';
-const socket = io(import.meta.env.VITE_BACKEND_URL, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
+
 export default function CommunityProfile({ comId }) {
   const [community, setCommunity] = useState(null);
   const [userId, setUserId] = useState(null);
